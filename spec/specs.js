3544c0a8c3b90a9c92f8656352e6aa092766a43d
matt knutson and sean burgoyne <mknut001@gmail.com>
2016-01-25 13:16:14 -0800

    Executed first spec for place function successfully.

--- spec/specs.js
+++ spec/specs.js
@@ -1,21 +1,22 @@
-<html>
-<head>
-  <title>Mocha spec runner</title>
-  <link rel="stylesheet" href="mocha.css">
-</head>
-<body>
-  <div id="mocha"></div>
-  <script src="../js/jquery-1.12.0.js"></script>
-  <script src="../js/scripts.js"></script>
-  <script src="mocha.js"></script>
-  <script src="chai.js"></script>
-  <script>mocha.setup('bdd');</script>
-  <script src="specs.js"></script>
-  <script>
-    expect = chai.expect;
-    mocha.checkLeaks();
-    mocha.globals(['jQuery']);
-    mocha.run();
-  </script>
-</body>
-</html>
+describe('Place', function() {
+  it("creates a new place with the given properties", function() {
+    var testPlace = new Place("France","Paris");
+    expect(testPlace.country).to.equal("France");
+    expect(testPlace.city).to.equal("Paris");
+    expect(testPlace.details).to.eql([]);
+  });
+
+  it("adds the fullName method to all contacts", function() {
+    var testContact = new Contact("Sherlock","Holmes");
+    expect(testContact.fullName()).to.equal("Sherlock Holmes");
+  });
+
+  describe('Address', function() {
+  it("creates a new address with the given specifications", function() {
+    var testAddress = new Address("123 Main St", "Test City","Test State");
+    expect(testAddress.street).to.equal("123 Main St");
+    expect(testAddress.city).to.equal("Test City");
+    expect(testAddress.state).to.equal("Test State");
+  });
+});
+});
